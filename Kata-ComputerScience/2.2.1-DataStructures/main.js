ff68a01307c0095cd43547f6f7a2be93a10127a8
Miguel <miguelcontreras56@aragon.unam.mx>
2024-10-15 10:37:46 -0600

    Creacion de la pila y sus funciones

--- Kata-ComputerScience/2.2.1-DataStructures/main.js
+++ Kata-ComputerScience/2.2.1-DataStructures/main.js
@@ -20,4 +20,32 @@ class Stack{
         this.#stack = newStack;
         return popedElement;
     }
+
+    peek() {
+        return this.#stack[this.#stack.length - 1];
+    }
+
+    size() {
+        return this.#stack.length;
+    }
+
+    print() {
+        let printedStack = "";
+        this.#stack.forEach((stackElement) => printedStack += `${stackElement}, `);
+        console.log(printedStack);
+    }
 }
+
+let myStack = new Stack();
+
+myStack.push(5);
+myStack.push(8);
+myStack.push(10);
+
+console.log(myStack);
+console.log(myStack.pop());
+console.log(myStack);
+console.log(myStack.peek());
+console.log(myStack);
+
+myStack.print();
