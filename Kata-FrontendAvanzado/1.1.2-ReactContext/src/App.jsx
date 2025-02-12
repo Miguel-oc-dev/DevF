5d306689d058a61f3e6410a4e078e85aea0318be
Miguel-oc-dev <miguelcontreras56@aragon.unam.mx>
2025-02-11 20:36:28 -0600

    ✨feat: Importamos Home

--- Kata-FrontendAvanzado/1.1.2-ReactContext/src/App.jsx
+++ Kata-FrontendAvanzado/1.1.2-ReactContext/src/App.jsx
@@ -1,11 +1,10 @@
+import Home from './Pages/Home'
 import './App.css'
-import Home from '../Pages/Home'
 
 function App() {
 
   return (
     <>
-    <h1>Hello World</h1>
       <Home/>
     </>
   )
