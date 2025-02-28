75b283c539864e7a9d92f52b61e158ade4bd7bf6
Miguel-oc-dev <miguelcontreras56@aragon.unam.mx>
2025-02-27 20:53:11 -0600

    🚩feat: Se mejora la visualizacion del codigo

--- Kata-FrontendAvanzado/1.1.3-ReactAuth/src/Components/Header.jsx
+++ Kata-FrontendAvanzado/1.1.3-ReactAuth/src/Components/Header.jsx
@@ -8,25 +8,25 @@ const Header = () => {
         <div>
         {/* BEM PARA NUESTRAS CLASES EN HTML */}
         {/* kebabcase para nombrar nuestras className en React con minúsculas */}
-        <nav className="header"> {/* BLOQUE */}
-            <NavLink className="header__logo" to="/">LOGO</NavLink>
-            <ul className="header__nav-list" />
-            <li className="header__list-item">
-                <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/home">Home</NavLink>
-            </li>
-            <li className="header__list-item">
-                <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/dashboard">Dashboard</NavLink>
-            </li>
-            <li className="header__list-item">
-                <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/secret">Secret</NavLink>
-            </li>
-            <li className="header__list-item">
-                <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/login">Login</NavLink>
-            </li>
-            <li className="header__list-item">
-                <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/signup">Signup</NavLink>
-            </li>
-        </nav>
+            <nav className="header"> {/* BLOQUE */}
+                <NavLink className="header__logo" to="/">LOGO</NavLink>
+                <ul className="header__nav-list" />
+                <li className="header__list-item">
+                    <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/home">Home</NavLink>
+                </li>
+                <li className="header__list-item">
+                    <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/dashboard">Dashboard</NavLink>
+                </li>
+                <li className="header__list-item">
+                    <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/secret">Secret</NavLink>
+                </li>
+                <li className="header__list-item">
+                    <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/login">Login</NavLink>
+                </li>
+                <li className="header__list-item">
+                    <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/signup">Signup</NavLink>
+                </li>
+            </nav>
         </div>
     )
 }
