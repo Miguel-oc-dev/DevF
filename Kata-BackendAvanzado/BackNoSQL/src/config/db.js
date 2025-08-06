421ea98a9257dbb818a4979b7491451f2c7dedb6
Miguel-oc-dev <miguelcontreras56@aragon.unam.mx>
2025-08-06 12:50:47 -0600

    feat: Uso de constantes

--- Kata-BackendAvanzado/BackNoSQL/src/config/db.js
+++ Kata-BackendAvanzado/BackNoSQL/src/config/db.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose"
+import {mongoDomain, mongoUser, mongoPwd, mongoDb} from './constans.js'
 
 const connectDB = async() => {
     try {
-        await mongoose.connect(":@/");
+        await mongoose.connect(`${mongoDomain}${mongoUser}:${mongoPwd}@${mongoDb}/`);
         console.log('MongoDB connect...')
     } catch (error) {
-        
+        console.log(error)
+        process.exit(1);
     }
 }
